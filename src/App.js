1b8273616cbcc4b11a30e5212d6ec6e851b94812
SharonNavarro <shanmowrk@gmail.com>
2022-08-03 18:32:30 -0500

    feat: count and search todo's

--- src/App.js
+++ src/App.js
@@ -5,23 +5,47 @@ import { TodoList } from "./TodoList.js";
 import { TodoItem } from "./TodoItem.js";
 import { CreateTodoButton } from "./CreateTodoButton.js";
 
-const todos = [
+const defaulTtodos = [
   { text: 'Cortar cebolla', completed: true },
-  { text: 'Toamr curso', completed: false },
+  { text: 'Toamr curso', completed: true },
   { text: 'Llorar', completed: false }
 ];
 
 function App() {
+  const [todos, setTodos] = React.useState(defaulTtodos);
+  const [searchValue, setSearchValue] = React.useState('');
+  const completedTodos = todos.filter(todo => todo.completed).length;
+  const totalTodos = todos.length;
+
+  let searchedTodos = [];
+
+  if (!searchValue.length >= 1) {
+    searchedTodos = todos;
+  } else {
+    searchedTodos = todos.filter(todo => {
+      const todoText = todo.text.toLowerCase();
+      const searchText = searchValue.toLowerCase();
+      return todoText.includes(searchText);
+    })
+  }
+
+
   return (
     <React.Fragment>
-      {<TodoCounter />}
-      {<TodoSearch />}
+      {<TodoCounter
+        total={totalTodos}
+        completed={completedTodos}
+      />}
+      {<TodoSearch
+        searchValue={searchValue}
+        setSearchValue={setSearchValue}
+      />}
       {<TodoList>
-        {todos.map(todo => (
-          <TodoItem 
-          key={todo.text} 
-          text={todo.text} 
-          completed={todo.completed}
+        {searchedTodos.map(todo => (
+          <TodoItem
+            key={todo.text}
+            text={todo.text}
+            completed={todo.completed}
           />
         ))}
       </TodoList>}
--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,18 +1,21 @@
 import React from "react";
 import "./TodoSearch.css";
 
-function TodoSearch(){
+function TodoSearch({searchValue, setSearchValue }) {
     const onSearchValueChange = (event) => {
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     };
 
-    return(
+    return[
         <input 
         className="TodoSearch" 
         placeholder="Cebolla" 
+        value={searchValue}
         onChange={onSearchValueChange}
-        />
-    );
+        />,
+        <p>{searchValue}</p>
+    ];
 }
 
-export {TodoSearch};
+export { TodoSearch };
